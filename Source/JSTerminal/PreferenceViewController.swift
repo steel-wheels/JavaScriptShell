2d9ad83a527b9a709f9ef8326da8f36531412d4c
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2026-06-14 18:25:24 +0900

    PreferenceViewController: Add widgets

--- Source/JSTerminal/PreferenceViewController.swift
+++ Source/JSTerminal/PreferenceViewController.swift
@@ -13,16 +13,59 @@ public class PreferenceViewController: NSViewController
         private weak var mRootView: MIStack? = nil
 
         public override func loadView() {
-                let stack = MIStack()
-                stack.wantsLayer = true
-                stack.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
-                mRootView = stack
+                let rootview = MIStack()
+                rootview.axis = .vertical
+                mRootView = rootview
 
-                self.view = stack
+                self.view = rootview
         }
+
+        private var rootView: MIStack { get {
+                if let root = mRootView {
+                        return root
+                } else {
+                        fatalError("root view must be allocated")
+                }
+        }}
 
         public override func viewDidLoad() {
                 super.viewDidLoad()
-                // allocate contents
+                allocateContents(rootVoew: self.rootView)
+        }
+
+        private func allocateContents(rootVoew root: MIStack) {
+                root.addArrangedSubView(self.allocateHomeDirectorySelector())
+        }
+
+        private func allocateHomeDirectorySelector() -> MIStack {
+                if let _ = currentViewController() {
+                        NSLog("has view controller")
+                }
+
+                let stack = MIStack()
+                stack.axis = .horizontal
+
+                let button = MIButton()
+                button.title = "Set Home Directory"
+                button.isEnabled = true
+                stack.addArrangedSubView(button)
+
+                return stack
+        }
+
+        private func currentViewController() -> ViewController? {
+                guard let doc = NSDocumentController.shared.currentDocument else {
+                        return nil
+                }
+                let vconts = doc.windowControllers
+                guard vconts.count > 0 else {
+                        return nil
+                }
+                if let vcont = vconts[0].contentViewController as? ViewController {
+                        return vcont
+                } else {
+                        return nil
+                }
         }
 }
+
